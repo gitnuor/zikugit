d51a3c540e81dd0f202e7fbdf06adf93b13591df
noor <noor.alam@bracits.com>
2022-09-03 19:53:07 +0600

    commit

--- src/noor.js
+++ src/noor.js
@@ -2,32 +2,32 @@ var msg='hello';
 console.log(msg);
 // function noor(){
 
-//  // return (msg);
+//   return (msg);
 // }
 
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
-// function noor() {
-//   return (
-//     <div className="App">
-//       {/* used for  */}
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function noor() {
+  return (
+    <div className="App">
+      {/* used for  */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
- //export default noor;
+ export default noor;
